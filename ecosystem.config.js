ae2d67eed7c1a7c4556e9bf112b70b3bc6815d8e
wmui <qq22337383@gmail.com>
2017-12-01 15:33:17 +0800

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'vueblog-server',
+      script    : 'server.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'root',
+      host : '198.13.32.165',
+      ref  : 'origin/master',
+      repo : 'git@github.com:wmui/vueblog-server.git',
+      path : '/www',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'root',
+      host : '198.13.32.165',
+      ref  : 'origin/master',
+      repo : 'git@github.com:wmui/vueblog-server.git',
+      path : '/www',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
